fcf9897a349669313ed5a88fd8378df68693b6fb
Tiernan8r <tiernan8r@pm.me>
2020-07-16 11:28:45 +0100

    fixed errors

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-        "github.com/tiernan8r/sudoku_solver/sudoku";
-        "fmt"
-        )
+    "fmt"
+    "github.com/tiernan8r/sudoku_solver/sudoku"
+)
 
 func main() {
     fmt.Println("HEllo WORLD!")
 
-    board_size := 9
-    cells := [board_size][board_size]Cell
+    const board_size int = 9
+    cells := [board_size][board_size]sudoku.Cell{}
 
-    sudoku_board := Grid{size: board_size, board: cells}
+    sudoku_board := sudoku.Grid{size: board_size, board: cells}
 
-    sudoku_board = Solve(sudoku_board)
+    sudoku_board = sudoku.Solve(sudoku_board)
 
     fmt.Println("FINITO!")
 
